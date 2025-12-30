3f0f3da901153d064f08e037de4cbaaf74df2b17
somildholakia <somildholakia0852@gmail.com>
2025-12-30 19:16:48 +0530

    update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com","www.epiclead.com","www.greatlead.com"]
 const inputbtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 
 
-
 inputbtn.addEventListener("click", function() {
-    myLeads.push("www.awesomelead.com")
-    console.log("lead pushed")
+
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
 
+for(let i=0;i<myLeads.length;i++){
+    console.log(myLeads[i])
+}
 
 
