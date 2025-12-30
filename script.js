124feaa195e77a5a1e35bd6a27cff8485edc705a
somildholakia <somildholakia0852@gmail.com>
2025-12-30 18:22:59 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
-function input_btn(){
-    console.log("button clicked")
-}
+let inputbtn = document.getElementById("input-btn")
+inputbtn.addEventListener("click", function() {
+    console.log("Button clicked from eventListener")
+})
