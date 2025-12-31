c50d0507f2725d01faa3990228ef481dad941f48
somildholakia <somildholakia0852@gmail.com>
2025-12-31 15:54:24 +0530

    update

--- script.js
+++ script.js
@@ -2,38 +2,49 @@ let myLeads = []
 const inputbtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-El")
+const deleteEl = document.getElementById("delete-btn")
 
+const leadsFromLocalStrorage = JSON.parse(localStorage.getItem("myLeads"))
 
-let leadsFromLocalStrorage = JSON.parse(localStorage.getItem("myLeads"))
-// localStorage.clear()
-
-if(leadsFromLocalStrorage){
-    myLeads.push(leadsFromLocalStrorage)
-    renderLeads()
-}
-
-inputbtn.addEventListener("click", function () {
-
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
-    renderLeads()
-
-    
+deleteEl.addEventListener("click", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    // location.reload()
 })
 
 
-function renderLeads() {
+if (leadsFromLocalStrorage) {
+    myLeads.push(leadsFromLocalStrorage)
+    render(myLeads)
+}
+
+
+function render(Leads) {
     let listItems = []
-    for (let i = 0; i < myLeads.length; i++) {
-     listItems += `<li>
-        <a href = ${myLeads} target ='_blank'>
-        ${myLeads[i]} </a> 
+    for (let i = 0; i < Leads.length; i++) {
+        listItems += `<li>
+        <a href = ${Leads} target ='_blank'>
+        ${Leads[i]} </a> 
         </li>`
     }
 
     ulEl.innerHTML = listItems
 
 }
+
+
+inputbtn.addEventListener("click", function () {
+
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads)
+
+
+})
+
+
+
